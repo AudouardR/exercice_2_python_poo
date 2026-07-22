d6d2fba038eb87df77887e15f42399ba86a37568
Arnaud Delperier <arnaud.delperier@fms-ea.com>
2026-07-22 13:11:41 +0200

    Amélioration de la méthode add_article pour ne pas ajouter de doublons

--- client.py
+++ client.py
@@ -7,11 +7,16 @@ class Client:
     def __init__(self, first_name: str, last_name: str):
         self.first_name = first_name
         self.last_name = last_name
-        self.purchases: list[tuple[str, int]] = []
+        self.purchases: list[dict[str, int]] = []
         self.clients.append(self)
 
     def __repr__(self):
         return self.first_name + " " + self.last_name + " : Achats : " + str(self.purchases)
 
     def add_article(self, product: Product, quantity: int):
-        self.purchases.append((product.name, quantity))
+        for purchase in self.purchases:
+            if product.name in purchase:
+                purchase[product.name] += quantity
+                return
+
+        self.purchases.append({product.name: quantity})
